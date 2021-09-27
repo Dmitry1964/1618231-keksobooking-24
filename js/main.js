6ca989dc6fd9941d701c0dae06be733e69563510
Dmitry1964 <nosdm64@gmail.com>
2021-09-27 11:29:35 +0300

    Исправляет ДЗ Нужно блльше функций

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@
 // Функции - генераторы случайных чисел
 // Источник - https://developer.mozilla.org
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   if (min >= max || min < 0) {
-    return 'Минимальное значение диапазона не может быть отрицательным, больше или равняться максимальному';
+    return -1;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-getRandomInt(2.2, 100.5);
+getRandomInt();
 
-function getRandomVar(min, max, decimalPlaces) {
+const getRandomVar = (min, max, decimalPlaces) => {
   if (min >= max || min < 0) {
-    return 'Минимальное значение диапазона не может быть отрицательным, больше или равняться максимальному';
+    return -1;
   }
   const number = Math.random() * (max - min) + min;
   return number.toFixed(decimalPlaces);
-}
+};
 
-getRandomVar(20, 100.25, 2);
+getRandomVar();
