1be754606654d8ac8203814f2f9d2f68ed0ff474
Dmitry1964 <nosdm64@gmail.com>
2021-10-31 20:42:42 +0300

    Помощь друга. Исправления и дополнения.

--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,7 @@
-import { baseHousings as offers } from './data.js';
 import { numDecline } from './utilits.js';
 
-const mapCanvas = document.querySelector('.map__canvas');
 const templateFragment = document.querySelector('#card').content;
 const template = templateFragment.querySelector('.popup');
-const fragment = document.createDocumentFragment();
 
 const getTypeHousing = (typeOffer) => {
   if (typeOffer === 'flat') {
@@ -19,11 +16,10 @@ const getTypeHousing = (typeOffer) => {
   return 'Отель';
 };
 
-const getFeaturesPopup = (index, listElement) => {
+const getFeaturesPopup = (features, listElement) => {
   const featuresItems = listElement.querySelectorAll('.popup__feature');
-  const featureModList = offers[index].offer.features;
   featuresItems.forEach((featuresItem) => {
-    const target = featureModList.some((featureModItem) => featuresItem.classList.contains(`popup__feature--${featureModItem}`),
+    const target = features.some((featureModItem) => featuresItem.classList.contains(`popup__feature--${featureModItem}`),
     );
     if (!target) {
       featuresItem.remove();
@@ -32,10 +28,9 @@ const getFeaturesPopup = (index, listElement) => {
   return featuresItems;
 };
 
-const getPhotosPopup = (index, listPhotos) => {
-  const photosList = offers[index].offer.photos;
+const getPhotosPopup = (photos, listPhotos) => {
   listPhotos.innerHTML = '';
-  photosList.forEach((photosItem) => {
+  photos.forEach((photosItem) => {
     const photosPopupItem = document.createElement('img');
     photosPopupItem.src = photosItem;
     photosPopupItem.width = '45';
@@ -61,22 +56,51 @@ const getCardsHousing = (arr) => {
   const descriptionPopup = element.querySelector('.popup__description');
   const photosPopupList = element.querySelector('.popup__photos');
 
-  (avatar) ? avatarPopup.src = arr.author.avatar : avatarPopup.classList.add('hidden');
-  (title) ? titlePopup.textContent = title : titlePopup.classList.add('hidden');
-  (address) ? addressPopup.textContent = address : addressPopup.classList.add('hidden');
-  (price) ? pricePopup.textContent = `${price} ₽/ночь` : pricePopup.classList.add('hidden');
-  (type) ? typePopup.textContent = getTypeHousing(type) : typePopup.classList.add('hidden');
-  (!rooms || !quests) ?
-    capacityPopup.classList.add('hidden') :
-    capacityPopup.textContent = `${rooms} ${numDecline(rooms, 'комната', 'комнаты', 'комнат')} для ${`${quests} ${numDecline(quests, 'гостя', 'гостей', 'гостей')}`} `;
-  (!checkin || !checkout) ?
-    timePopup.classList.add('hidden') :
+  if (avatar) {
+    avatarPopup.src = arr.author.avatar;
+  } else { avatarPopup.classList.add('hidden'); }
+
+  if (title) {
+    titlePopup.textContent = title;
+  } else { titlePopup.classList.add('hidden'); }
+
+  if (address) {
+    addressPopup.textContent = address;
+  } else { addressPopup.classList.add('hidden'); }
+
+  if (price) {
+    pricePopup.textContent = `${price} ₽/ночь`;
+  } else { pricePopup.classList.add('hidden'); }
+
+  if (type) {
+    typePopup.textContent = getTypeHousing(type);
+  } else { typePopup.classList.add('hidden'); }
+
+  if (!rooms || !quests) {
+    capacityPopup.classList.add('hidden');
+  } else {
+    capacityPopup.textContent = `${rooms} ${numDecline(rooms, 'комната', 'комнаты', 'комнат')} для ${`${quests}
+   ${numDecline(quests, 'гостя', 'гостей', 'гостей')}`} `;
+  }
+
+  if (!checkin || !checkout) {
+    timePopup.classList.add('hidden');
+  } else {
     timePopup.textContent = `Заезд после ${checkin} , выезд до${checkout}`;
-  (features.length !== 0) ? getFeaturesPopup(0, featuresList) : featuresList.classList.add('hidden');
-  (description) ? descriptionPopup.textContent = description : descriptionPopup.classList.add('hidden');
-  (photos.length !== 0) ? getPhotosPopup(0, photosPopupList) : photosPopupList.classList.add('hidden');
+  }
+
+  if (features.length !== 0) {
+    getFeaturesPopup(features, featuresList);
+  } else { featuresList.classList.add('hidden'); }
+
+  if (description) {
+    descriptionPopup.textContent = description;
+  } else { descriptionPopup.classList.add('hidden'); }
+
+  if (photos.length !== 0) {
+    getPhotosPopup(photos, photosPopupList);
+  } else { photosPopupList.classList.add('hidden'); }
   return element;
 };
 
-mapCanvas.appendChild(fragment);
 export { getCardsHousing };
--- js/map.js
+++ js/map.js
@@ -1,39 +1,46 @@
-import { setDisabledForms, setDisabledFields } from './form.js';
+import { setDisabledForms, setDisabledFields, resetButton } from './form.js';
 import { baseHousings as offers } from './data.js';
 import { getCardsHousing } from './cards.js';
 
 const DECIMAL_PLACES = 5;
+const ZOOM = 12;
+const ICON_SIZE = [52, 52];
+const ICON_ANCHOR = [26, 52];
+const MAP_ADDRESS = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const downTown = {
+  lat: 35.65283,
+  lng: 139.83947,
+};
+const mapAttribution = {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+};
+
 const addressOnMap = document.querySelector('#address');
 
 const map = L.map('map-canvas')
+  .on('load', () => {
+    setDisabledForms();
+    setDisabledFields();
+  })
   .setView({
-    lat: 35.65283,
-    lng: 139.83947,
-  }, 12);
+    lat: downTown.lat,
+    lng: downTown.lng,
+  }, ZOOM);
 
-// map.on('load', () => {
-//   setDisabledFields();
-//   setDisabledForms();
-// });
-
-setDisabledFields();
-setDisabledForms();
-
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+L.tileLayer(MAP_ADDRESS,
+  { mapAttribution },
+).addTo(map);
 
 const iconDefault = L.icon({
   iconUrl: 'img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
+  iconSize: ICON_SIZE,
+  iconAnchor: ICON_ANCHOR,
 });
 
 const markerDefault = L.marker(
   {
-    lat: 35.65283,
-    lng: 139.83947,
+    lat: downTown.lat,
+    lng: downTown.lng,
   },
   {
     draggable: true,
@@ -53,18 +60,19 @@ markerDefault.on('moveend', (evt) => {
   getAddressOnMap(evt.target.getLatLng());
 });
 
-map.on('click', () => {
+const setMarkerDefault = () => {
   markerDefault.setLatLng({
-    lat: 35.65283,
-    lng: 139.83947,
+    lat: downTown.lat,
+    lng: downTown.lng,
   });
   addressOnMap.value = `${markerDefault._latlng.lat}, ${markerDefault._latlng.lng}`;
-});
+};
+
 
 const iconPoint = L.icon({
   iconUrl: 'img/pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
+  iconSize: ICON_SIZE,
+  iconAnchor: ICON_ANCHOR,
 });
 
 const getPoints = (item) => {
@@ -85,4 +93,16 @@ const getPoints = (item) => {
 offers.forEach((element) => {
   getPoints(element);
 });
+
+
+resetButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  setMarkerDefault();
+  map.setView({
+    lat: downTown.lat,
+    lng: downTown.lng,
+  }, ZOOM);
+  map.closePopup();
+});
+
 export { map };
